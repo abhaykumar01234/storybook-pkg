b270184107d5655964adbe628ca8c2a9f9ad63bd
abhaykumar01234 <abhaykumar01234@users.noreply.github.com>
2024-08-14 05:11:22 +0000

    deploy: 0c9d1eca8e889dced0ffe9175b3a8c31ec4f2885

---
+++ assets/Button.stories-DuRqjIhl.js
@@ -0,0 +1,6 @@
+import{j as i}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const n=({style:a={},...s})=>i.jsx("button",{style:{...a,background:"darkred",color:"white",padding:"0.5rem 1.5rem",border:"none",outline:"none",borderRadius:"4px",cursor:"pointer",fontWeight:"600"},...s});n.__docgenInfo={description:"",methods:[],displayName:"Button",props:{style:{defaultValue:{value:"{}",computed:!1},required:!1}}};const u={title:"Button",component:n},e={args:{children:"My Button",onClick:()=>alert("My name is Abhay")}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: "My Button",
+    onClick: () => alert("My name is Abhay")
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const m=["Primary"];export{e as Primary,m as __namedExportsOrder,u as default};
---
+++ assets/Radio.stories-CqdW-4sy.js
@@ -0,0 +1,7 @@
+import{j as c}from"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const s=({style:o={},...d})=>c.jsx("input",{type:"radio",style:{...o,accentColor:"darkred",scale:"1.2"},...d});s.__docgenInfo={description:"",methods:[],displayName:"Radio",props:{style:{defaultValue:{value:"{}",computed:!1},required:!1}}};const i={title:"Radio",component:s},e={args:{name:"category",checked:!0,disabled:!1}};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    name: "category",
+    checked: true,
+    disabled: false
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,i as default};
